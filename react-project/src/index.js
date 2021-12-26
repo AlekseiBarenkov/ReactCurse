3cf6869b3b0581f5d89bf9f5f66688a745cc3a27
Aleks <barenkov_aleksei@mail.ru>
2021-12-26 22:04:01 +0500

    first homework in react-project

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+/* ================ДЗ от 26.12.2021 (Компоненты, JSX, рендеринг)================*/
+//!Функциональный компонент
+function ContainerFunc (props) {
+  let answer = props.answer;
+  if (answer === 4) {
+    return(
+      <div>
+        <h1>Сколько будет 2 + 2</h1>
+        <h2>Твой ответ: {props.answer}. И это правильный ответ!!!</h2>
+     </div>
+    )
+  }
+  return (
+    <div>
+        <h1>Сколько будет 2 + 2</h1>
+        <h2>Твой ответ: {props.answer}. Это не правильный ответ, попробуй снова</h2>
+    </div>
+  )
+}
+
+//!Классовый компонент
+class ContainerClass extends React.Component {
+  render () {
+    let answer = this.props.answer;
+    if (answer === 4) {
+      return(
+        <div>
+          <h1>Сколько будет 2 + 2</h1>
+          <h2>Твой ответ: {this.props.answer}. И это правильный ответ!!!</h2>
+      </div>
+      )
+    }
+    return (
+      <div>
+          <h1>Сколько будет 2 + 2</h1>
+          <h2>Твой ответ: {this.props.answer}. Это не правильный ответ, попробуй снова</h2>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <App />,
+  <ContainerClass answer= {4}/>,
   document.getElementById('root')
 );
